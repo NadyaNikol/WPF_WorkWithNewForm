2b4fe4d339b2005958665e0b88e3be5f838723c4
Kerusak <kerusm1497@gmail.com>
2019-06-13 21:03:30 +0300

    ItIsNotAll

---
+++ WpfApp1/WpfApp1/ChildWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for ChildWindow.xaml
+    /// </summary>
+    public partial class ChildWindow : Window
+    {
+        Employee employee;
+        public ChildWindow( Employee emp)
+        {
+            InitializeComponent();
+            this.employee = emp;
+       
+        }
+
+        private void ButtonOK_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonAddPhoto_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            if (openDialog.ShowDialog() == true)
+            {
+                //Image image = new Image();
+                
+                //(this.Content as Panel).Children.Add(image);
+
+                Grid mainGrid = this.Content as Grid;
+                foreach (UIElement elem in mainGrid.Children)
+                {
+                    if (elem is Grid)
+                    {
+                        Grid childGrid = elem as Grid;
+                        foreach (UIElement childElem in childGrid.Children)
+                        {
+                            if (childElem is Image)
+                            {
+                                Image image = childElem as Image;
+                                image.Source = new BitmapImage(new Uri(openDialog.FileName, UriKind.RelativeOrAbsolute));
+                            }
+                        }
+                            //Grid.SetRowSpan(image, 6);                       
+                    }
+                }
+              
+            }
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfApp1
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public int Salary { get; set; }
+        public string Birthday { get; set; }
+    }
+}
